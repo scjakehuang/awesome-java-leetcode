e33d653d940bbdcaf3aa45e953d38c5d741b3bc1
wb-hjk570755 <wb-hjk570755@koubei.com>
2020-04-03 14:59:32 +0800

    动态规划

--- src/com/blankj/easy/_053/Solution.java
+++ src/com/blankj/easy/_053/Solution.java
@@ -45,4 +45,15 @@ public class Solution {
         int[] nums0 = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(solution.maxSubArray(nums0));
     }
+
+    public int maxSubArrayByD(int[] nums) {
+        int len = nums.length, dp = nums[0], max = dp;
+        for (int i = 1; i < len; ++i) {
+            dp = nums[i] + (dp > 0 ? dp : 0);
+            if (dp > max) {
+                max = dp;
+            }
+        }
+        return max;
+    }
 }
